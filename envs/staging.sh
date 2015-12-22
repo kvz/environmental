f59966acb6be2bc01ee68d55b134a9de64076771
Luis Gomez <lgomez@gmail.com>
2015-12-21 18:33:13 -0800

    Making it work on zsh

--- envs/staging.sh
+++ envs/staging.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__dir="$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)"
 source ${__dir}/production.sh
 # Based on production ---^
 
-export DEPLOY_ENV="$(basename "${BASH_SOURCE[0]}" .sh)"
+export DEPLOY_ENV="$(basename "${BASH_SOURCE:-$0}" .sh)"
 #      ^--- But DEPLOY_ENV is still staging
--- envs/test.sh
+++ envs/test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__dir="$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)"
 source ${__dir}/development.sh
 # Based on development ---^
 
-export DEPLOY_ENV="$(basename "${BASH_SOURCE[0]}" .sh)"
+export DEPLOY_ENV="$(basename "${BASH_SOURCE:-$0}" .sh)"
 export NODE_ENV="test"
 export DEBUG=*:*
